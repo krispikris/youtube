d8cdadc183d217a616a06e82bf41a2713269b150
krispikris <kris@kristopherhan.com>
2023-12-26 01:28:38 -0800

    app

---
+++ resources/js/Layouts/NavLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { usePage, Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+   
+<div class="relative">
+    <div id="TopNav"></div>
+    <div id="SideNav"></div>
+    <div id="SideNavOverlay"></div>
+    <slot />
+</div>
+</template>
